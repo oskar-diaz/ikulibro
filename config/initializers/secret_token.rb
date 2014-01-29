c569fef036bc2acd04cfdcabbf0cf52d9410f281
Oskar <koki142@gmail.com>
2014-01-29 09:09:50 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ikulibro::Application.config.secret_key_base = '0921cb5840b6e3fde5c574a03e61c8a15bced7c5546d53c4af79cc2a6ceb3ca01cc99a3179f977dd4f589d76d3aa10b0b94dc791338421f0387dd28c93181777'
