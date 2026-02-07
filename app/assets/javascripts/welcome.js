5443475ff6ce349ea79b8ea8890bc8630f12f1d0
Oskar <koki142@gmail.com>
2026-02-07 11:07:27 +0900

    Añadidas mas tiendas

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -38,6 +38,46 @@ $(function () {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   });
 
+  var setupRandomHover = function ($btn) {
+    var $variants = $btn.find(".icon-variant");
+    if ($variants.length === 0) {
+      return;
+    }
+
+    var setRandomVariant = function () {
+      var prevIdx = $btn.data("variantIndex");
+      var idx = Math.floor(Math.random() * $variants.length);
+      if ($variants.length > 1) {
+        while (idx === prevIdx) {
+          idx = Math.floor(Math.random() * $variants.length);
+        }
+      }
+      $variants.removeClass("active");
+      $variants.eq(idx).addClass("active");
+      $btn.addClass("show-variant");
+      $btn.data("variantIndex", idx);
+    };
+
+    setRandomVariant();
+
+    $btn.on("mouseenter", function () {
+      setRandomVariant();
+      $btn.addClass("hovering");
+    });
+
+    $btn.on("mouseleave", function () {
+      $btn.removeClass("hovering");
+    });
+  };
+
+  $(".amazon-btn.random-hover").each(function () {
+    setupRandomHover($(this));
+  });
+
+  $(".amazon-btn.disabled").on("click", function (e) {
+    e.preventDefault();
+  });
+
   setInterval(function () {
     $("#reacciones").removeClass("bounceInRight").toggleClass("tada");
   }, 5000);
